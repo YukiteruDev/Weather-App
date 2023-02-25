13c351e167a2ec3adeaa87dcdcfd1763dfb2ed38
Yukiteno <yunomyworld@gmail.com>
2023-02-25 15:07:37 +0800

    adjust html elements and styles

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/Panel.module.css";
 
 export default function Panel() {
-  return <section className={styles.panel}></section>;
+  return <main className={styles.panel}></main>;
 }

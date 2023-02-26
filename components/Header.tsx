7f89ac582419685e2162a08694e48721e11266b1
Yukiteno <yunomyworld@gmail.com>
2023-02-26 15:01:53 +0800

    add current weather info

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/Header.module.css";
+import { Icon } from "@iconify/react";
 import { lato } from "@/api/fonts";
 import Image from "next/image";
 import HeaderIcon from "@/public/icons/fill/rain.svg";
@@ -16,9 +17,39 @@ export default function Header() {
       <Image src={HeaderIcon} alt="header-icon" className={styles.icon} />
       <div className={styles.temperature}>
         <p className={lato.className}>
-          12<i>℃</i>
+          12<i>°</i>
         </p>
         <p className={styles.status}>Rain and snow mixed</p>
+        <div className={styles.bottom}>
+          <div className={styles.bottomInfo}>
+            <Icon icon="ph:t-shirt-bold" />
+            <div>
+              <span>Feel like</span>
+              <p>10°</p>
+            </div>
+          </div>
+          <div className={styles.bottomInfo}>
+            <Icon icon="material-symbols:visibility-outline" />
+            <div>
+              <span>Wind Speed</span>
+              <p>10km/h</p>
+            </div>
+          </div>
+          <div className={styles.bottomInfo}>
+            <Icon icon="material-symbols:visibility-outline" />
+            <div>
+              <span>Visibility</span>
+              <p>22km</p>
+            </div>
+          </div>
+          <div className={styles.bottomInfo}>
+            <Icon icon="material-symbols:visibility-outline" />
+            <div>
+              <span>Pressure</span>
+              <p>700hPa</p>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );

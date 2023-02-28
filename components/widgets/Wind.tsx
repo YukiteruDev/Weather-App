b59329f870516cb3cdf71947e7788a08ee856f59
Yukiteno <yunomyworld@gmail.com>
2023-02-28 21:51:31 +0800

    apply style for wind component

--- components/widgets/Wind.tsx
+++ components/widgets/Wind.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Icon from "public/icons/fill/wind-beaufort-5.svg";
+import styles from "styles/widgets/Temperature.module.css";
 
 export default function Wind() {
   return (
     <div className="widget">
       <h3>Wind Speed</h3>
-      <div>
-        <Image src={Icon} alt="icon" />
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Image src={Icon} alt="icon" />
+          <p>21km</p>
+        </div>
+        <p>Southwest, 260°</p>
       </div>
-      <p>21km/h, Southwest</p>
     </div>
   );
 }

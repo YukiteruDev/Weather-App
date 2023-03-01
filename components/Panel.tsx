1443004535d6be160df9125ad2775bf366a27b55
Yukiteno <yunomyworld@gmail.com>
2023-03-01 10:52:43 +0800

    use grid layout on panel section

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -20,29 +20,31 @@ export default function Panel() {
           <button className={styles.textButton}>System</button>
         </div>
       </div>
-      <section className={styles.forecast}>
-        <div className={styles.forecastButtons}>
-          <button className={styles.textButton}>Hourly Forecast</button>
-          <button className={styles.textButton}>Weekly Forecast</button>
-        </div>
-        <div className={styles.days}>
-          {range.map(num => {
-            return <Day key={num} />;
-          })}
-        </div>
-      </section>
-      <section className={styles.detail}>
-        <h2>Weather Details</h2>
-        <div className={styles.widgets}>
-          <Sunrise isRise={true} />
-          <Sunrise isRise={false} />
-          <Temperature highOrLow="high" />
-          <Temperature highOrLow="low" />
-          <Precipitation />
-          <PrecipitationSum />
-          <UVIndex level={3} />
-          <Wind />
-        </div>
+      <section className={styles.content}>
+        <article className={styles.forecast}>
+          <div className={styles.forecastButtons}>
+            <button className={styles.textButton}>Hourly Forecast</button>
+            <button className={styles.textButton}>Weekly Forecast</button>
+          </div>
+          <div className={styles.days}>
+            {range.map(num => {
+              return <Day key={num} />;
+            })}
+          </div>
+        </article>
+        <article className={styles.detail}>
+          <h2>Weather Details</h2>
+          <div className={styles.widgets}>
+            <Sunrise isRise={true} />
+            <Sunrise isRise={false} />
+            <Temperature highOrLow="high" />
+            <Temperature highOrLow="low" />
+            <Precipitation />
+            <PrecipitationSum />
+            <UVIndex level={3} />
+            <Wind />
+          </div>
+        </article>
       </section>
     </main>
   );

9e3838e206c4c8d41b1431d647f42945aff98c70
Yukiteno <yunomyworld@gmail.com>
2023-03-01 10:17:45 +0800

    add style for precipitation component

--- components/widgets/Precipitation.tsx
+++ components/widgets/Precipitation.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Icon from "public/icons/fill/raindrops.svg";
+import styles from "styles/widgets/Precipitation.module.css";
 
 export default function Precipitation() {
   return (
     <div className="widget">
-      <h3>Precipitation</h3>
-      <div>
-        <Image src={Icon} alt="icon" />
-        <p>0.50mm</p>
+      <h3>Precipitation Probability</h3>
+      <div className={styles.container}>
+        <Image src={Icon} alt="icon" className={styles.icon} />
+        <p>23%</p>
       </div>
     </div>
   );

cd8c491814c2a0682646d6c1898f808d8227b622
Yukiteno <yunomyworld@gmail.com>
2023-03-09 11:31:54 +0800

    create getDailyForecast function

--- api/request.ts
+++ api/request.ts
@@ -1,33 +1,54 @@
 const baseUrl = "https://api.open-meteo.com/v1/forecast";
-const [latitude, longitude] = ["35.65", "139.84"];
 
 type QueryType = {
   latitude: string;
   longitude: string;
-  start_date: string;
-  end_date: string;
   hourly?: string;
+  daily?: string;
+  start_date?: string;
+  end_date?: string;
 };
 
-function getQueryString(queryObject: QueryType) {
-  const queryString = new URLSearchParams(queryObject).toString();
-  return queryString.replace("%2C", ",");
+function getDateRange() {
+  const date = new Date();
+  const startDate = date.toISOString().slice(0, 10);
+  const tomorrow = new Date(date);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const endDate = tomorrow.toISOString().slice(0, 10);
+  return [startDate, endDate];
+}
+
+function getQueryString(hourly = "", daily = "") {
+  const [latitude, longitude] = ["35.65", "139.84"];
+  const queryObject: QueryType = { latitude, longitude };
+  if (hourly) {
+    queryObject.hourly = hourly;
+    [queryObject.start_date, queryObject.end_date] = getDateRange();
+  } else {
+    queryObject.daily = daily;
+  }
+  const params = new URLSearchParams(queryObject);
+  const queryString = params.toString().replace("%2C", ",");
+  return queryString;
 }
 
 async function getHourlyForecast() {
-  const queryObject: QueryType = {
-    latitude,
-    longitude,
-    hourly: "temperature_2m,weathercode",
-    start_date: "2023-03-08",
-    end_date: "2023-03-09",
-  };
-  const queryString = getQueryString(queryObject);
+  const queryString = getQueryString("temperature_2m,weathercode");
+  console.log(queryString);
+  const data = await sendRequest(queryString);
+  return data;
+}
+
+async function getDailyForecast() {
+  const queryString = getQueryString("", "temperature_2m,weathercode");
+  const data = await sendRequest(queryString);
+  return data;
+}
+
+async function sendRequest(queryString: string) {
   const requestUrl = `${baseUrl}?${queryString}`;
-  const res = await fetch(requestUrl, {
-    method: "GET",
-  });
+  const res = await fetch(requestUrl);
   return res.json();
 }
 
-export { getHourlyForecast };
+export { getHourlyForecast, getDailyForecast };

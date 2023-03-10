98d294d50ca88d567b90c12a771564155fe58b0d
Yukiteno <yunomyworld@gmail.com>
2023-03-10 10:14:54 +0800

    use dynamic icon for hour and daily components

--- components/widgets/Day.tsx
+++ components/widgets/Day.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import styles from "@/styles/Day.module.css";
-import Icon from "@/public/icons/fill/rain.svg";
 import { DailyData } from "@/types/temperature";
 import { dateIsToday, getDayOfWeek } from "@/api/date";
+import { getWeatherInfo } from "@/api/weatherCodes";
 
 type DailyProps = {
   data: DailyData;
@@ -16,6 +16,8 @@ interface CallbackInterface {
 export default function Day({ data, index, isActive, callback }: DailyProps) {
   const weekDay = getDayOfWeek(data.time[index]);
   const dayName = dateIsToday(data.time[index]) ? "Today" : weekDay;
+  const weatherInfo = getWeatherInfo(data.weathercode[index]);
+  const weatherIcon = require(`public/icons/fill/${weatherInfo.icon}.svg`);
   return (
     <div
       className={`${styles.container} ${isActive ? styles.active : ""}`}
@@ -25,7 +27,7 @@ export default function Day({ data, index, isActive, callback }: DailyProps) {
         <p>{dayName}</p>
         <span>{data.time[index]}</span>
       </div>
-      <Image src={Icon} alt="icon" className={styles.icon} />
+      <Image src={weatherIcon} alt="icon" className={styles.icon} />
       <div className={styles.weather}>
         <span>{data.temperature_2m_max[index]}°</span>
         <span>{data.temperature_2m_min[index]}°</span>
--- components/widgets/Hour.tsx
+++ components/widgets/Hour.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import styles from "@/styles/Hour.module.css";
-import Icon from "@/public/icons/fill/rain.svg";
 import { HourlyData } from "@/types/temperature";
+import { getWeatherInfo } from "@/api/weatherCodes";
 
 type HourProps = { data: HourlyData; index: number };
 export default function Hour({ data, index }: HourProps) {
+  const weatherInfo = getWeatherInfo(data.weathercode[index]);
+  const weatherIcon = require(`public/icons/fill/${weatherInfo.icon}.svg`);
   return (
     <div className={styles.container}>
       <p className={styles.time}>{data.time[index].slice(-5)}</p>
-      <Image src={Icon} alt="icon" className={styles.icon} />
+      <Image src={weatherIcon} alt="icon" className={styles.icon} />
       <p className={styles.weather}>{data.temperature_2m[index]}°</p>
     </div>
   );

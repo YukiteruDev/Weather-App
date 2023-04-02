8aae710153e9d21f328c5218d533efca9542b35b
Yukiteno <yunomyworld@gmail.com>
2023-04-02 16:06:04 +0800

    use dayjs to parse hour string in hour widget

--- components/widgets/Hour.tsx
+++ components/widgets/Hour.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 import styles from "@/styles/Hour.module.css";
 import { HourlyData } from "@/types/temperature";
 import { getWeatherIcon, getWeatherInfo } from "@/api/weatherCodes";
+import dayjs from "dayjs";
 
 type HourProps = { data: HourlyData; index: number };
 export default function Hour({ data, index }: HourProps) {
   const weatherInfo = getWeatherInfo(data.weathercode[index]);
   const weatherIcon = getWeatherIcon(weatherInfo.icon);
+  function getHour(date: string) {
+    return dayjs(date).format("HH:mm");
+  }
   return (
     <div className={styles.container}>
-      <p className={styles.time}>{data.time[index].slice(-5)}</p>
+      <p className={styles.time}>{getHour(data.time[index])}</p>
       <Image src={weatherIcon} alt="icon" className={styles.icon} />
       <p className={styles.weather}>{data.temperature_2m[index]}°</p>
     </div>
